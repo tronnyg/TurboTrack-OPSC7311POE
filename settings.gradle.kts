36c5a03771ff2c855b28cd163e3bf3b3ca9df676
Almighty Tronny <tronnyg@gmail.com>
2024-04-24 12:55:18 +0200

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "OPSC7311-POE"
+include(":app")
