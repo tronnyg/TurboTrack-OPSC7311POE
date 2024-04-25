a21540deeb0cbcbe9ac26b17d66a40b5e02b79cc
Almighty Tronny <tronnyg@gmail.com>
2024-04-25 15:35:08 +0200

    - Fixed code in MainActivity.kt

--- app/src/main/java/com/yugen/opsc7311_poe/MainActivity.kt
+++ app/src/main/java/com/yugen/opsc7311_poe/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.yugen.opsc7311_poe
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_page)
