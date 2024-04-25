1e0efd03586603a39d260b9157b4f4f12c3f5d6a
ST10132589 <blakemarais@gmail.com>
2024-04-25 14:59:43 +0200

    Got the main activity to display the welcome page, image not displaying properly

--- app/src/main/java/com/yugen/opsc7311_poe/MainActivity.kt
+++ app/src/main/java/com/yugen/opsc7311_poe/MainActivity.kt
@@ -18,33 +18,3 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.welcome_page)
     }
 }
-
-        /*{
-            OPSC7311_POETheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OPSC7311_POETheme {
-        Greeting("Android")
-    }
-}*/
