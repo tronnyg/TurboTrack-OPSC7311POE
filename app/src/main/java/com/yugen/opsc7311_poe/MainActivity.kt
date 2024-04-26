df40cc0ae4484a517457b35f7f35f13174c11a55
Almighty Tronny <tronnyg@gmail.com>
2024-04-26 16:06:17 +0200

    - Added fragment manager code in MainActivity.kt - Added timer_icon.xml - Changed bottom_navbar.xml

--- app/src/main/java/com/yugen/opsc7311_poe/MainActivity.kt
+++ app/src/main/java/com/yugen/opsc7311_poe/MainActivity.kt
@@ -2,12 +2,33 @@ package com.yugen.opsc7311_poe
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.yugen.opsc7311_poe.databinding.MainPageBinding
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : MainPageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = MainPageBinding.inflate(layoutInflater)
+        binding = MainPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomePageFragment())
+                else ->{}
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment)
+    {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
 
