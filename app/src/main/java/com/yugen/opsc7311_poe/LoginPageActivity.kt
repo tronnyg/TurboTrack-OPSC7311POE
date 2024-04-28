12e77d5d08d49341e3ba4402c2a6cc2b9e7b1c96
ST10132589 <blakemarais@gmail.com>
2024-04-28 16:26:48 +0200

    Added basic Login Validation and changed the text in input fields to hints

--- app/src/main/java/com/yugen/opsc7311_poe/LoginPageActivity.kt
+++ app/src/main/java/com/yugen/opsc7311_poe/LoginPageActivity.kt
@@ -1,9 +1,11 @@
 package com.yugen.opsc7311_poe
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.yugen.opsc7311_poe.Helpers.openIntent
 import com.yugen.opsc7311_poe.databinding.LoginPageBinding
+
 class LoginPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +15,22 @@ class LoginPageActivity : AppCompatActivity() {
             openIntent(this,CreateAccountActivity::class.java)
         }
         binding.buttonSignin.setOnClickListener{
-            openIntent(this, MainActivity::class.java)
+            val inputEmail =  binding.inputEmail.text.toString()
+            val inputPassword = binding.inputPassword.text.toString()
+
+            if (inputEmail.isEmpty() || inputPassword.isEmpty()) {
+                // Display a toast message indicating invalid email/password
+                Toast.makeText(this, "Invalid Email or Password, Please Try Again.", Toast.LENGTH_SHORT).show()
+            }
+            else if(inputEmail == "admin@gmail.com" && inputPassword == "admin")
+            {
+                openIntent(this, MainActivity::class.java)
+            }
+            else
+            {
+                Toast.makeText(this, "Invalid Email or Password, Please Try Again.", Toast.LENGTH_SHORT).show()
+            }
+
         }
     }
 }
