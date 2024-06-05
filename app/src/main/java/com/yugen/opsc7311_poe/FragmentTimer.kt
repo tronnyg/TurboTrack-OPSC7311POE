3f41756d8f3503e355e25133fdc2ead50c13c307
Aidan537 <st10061537@vcconnect.edu.za>
2024-06-05 15:37:20 +0200

    - added more colors to colors.xml - added more variables and methods to FragmentTimer.kt to get it working

--- app/src/main/java/com/yugen/opsc7311_poe/FragmentTimer.kt
+++ app/src/main/java/com/yugen/opsc7311_poe/FragmentTimer.kt
@@ -1,8 +1,6 @@
 package com.yugen.opsc7311_poe
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
@@ -13,19 +11,16 @@ import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentTimer.newInstance] factory method to
- * create an instance of this fragment.
- */
+enum class TimerState {
+    FOCUS, SHORT_BREAK, LONG_BREAK
+}
+
 class FragmentTimer : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
     private var timer: CountDownTimer? = null
@@ -37,6 +32,13 @@ class FragmentTimer : Fragment() {
     private var shortBreakTime = 0
     private var longBreakTime = 0
     private var pomoCycleCount = 0
+    private var cycleCount = 0
+    private lateinit var timerDisplay: TextView
+    private var timerState = TimerState.FOCUS
+    private var completedFocusSessions = 0
+    private var initialFocusTime = 0
+    private lateinit var playButton: RelativeLayout
+    private lateinit var playButtonImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +52,6 @@ class FragmentTimer : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_timer, container, false)
 
         val sharedPref = activity?.getPreferences(Context.MODE_PRIVATE)
@@ -60,26 +61,23 @@ class FragmentTimer : Fragment() {
         longBreakTime = sharedPref?.getInt("longBreakTime", 15) ?: 15
         pomoCycleCount = sharedPref?.getInt("pomoCycleCount", 2) ?: 2
 
-        val timerDisplay: TextView = view.findViewById(R.id.timerDisplay)
+        timerDisplay = view.findViewById(R.id.timerDisplay)
         timerDisplay.text = String.format("%02d\n00", focusTime)
 
-        // Find the timer_settings_button and set an OnClickListener
         val timerSettingsButton: RelativeLayout = view.findViewById(R.id.timer_settings_button)
         timerSettingsButton.setOnClickListener {
-            // Create an Intent to start EditTimerActivity
-           replaceFragment(FragmentTimerSettings())
+            replaceFragment(FragmentTimerSettings())
         }
 
-        val playButton: RelativeLayout = view.findViewById(R.id.play_button)
-        val playButtonImage: ImageView = playButton.findViewById(R.id.play_icon)
+        val skipButton: RelativeLayout = view.findViewById(R.id.timer_skip_button)
+        skipButton.setOnClickListener {
+            skipTimer()
+        }
 
-        // Get the time from timerDisplay
-        val timeString = timerDisplay?.text.toString().split("\n")
-        val minutes = timeString[0].toInt()
-        val seconds = timeString[1].toInt()
+        playButton = view.findViewById(R.id.play_button)
+        playButtonImage = playButton.findViewById(R.id.play_icon)
 
-        // Convert time to milliseconds and assign it to timeLeftInMillis
-        timeLeftInMillis = (focusTime*60) * 1000L
+        timeLeftInMillis = (focusTime * 60) * 1000L
 
         playButton.setOnClickListener {
             if (isTimerRunning) {
@@ -97,39 +95,78 @@ class FragmentTimer : Fragment() {
         return view
     }
 
-    private fun replaceFragment(fragment: Fragment)
-    {
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = requireActivity().supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
     }
 
-    private fun startTimer(timeInMillis: Long) {
-        timer = object: CountDownTimer(timeInMillis, 1000) {
+    private fun startTimer(duration: Long) {
+        timer = object : CountDownTimer(duration, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 timeLeftInMillis = millisUntilFinished
-                val remainingMinutes = millisUntilFinished / 1000 / 60
-                val remainingSeconds = millisUntilFinished / 1000 % 60
-                val timerDisplay: TextView? = view?.findViewById(R.id.timerDisplay)
-                timerDisplay?.text = String.format("%02d\n%02d", remainingMinutes, remainingSeconds)
+                updateCountDownText()
             }
 
             override fun onFinish() {
                 timer = null
-                currentPomoCount++
-                if (currentPomoCount == pomoCount) {
-                    timeLeftInMillis = (longBreakTime * 60) * 1000L
-                    currentPomoCount = 0
-                } else {
-                    timeLeftInMillis = (shortBreakTime * 60) * 1000L
+                when (timerState) {
+                    TimerState.FOCUS -> {
+                        completedFocusSessions++
+                        if (completedFocusSessions % pomoCount == 0) {
+                            if (cycleCount < pomoCycleCount - 1) {
+                                timerState = TimerState.LONG_BREAK
+                                startTimer(longBreakTime * 60 * 1000L)
+                                cycleCount++
+                            } else {
+                                Toast.makeText(context, "All cycles completed!", Toast.LENGTH_SHORT).show()
+                                timerState = TimerState.FOCUS
+                                pauseTimer()
+                                focusTime = initialFocusTime
+                                cycleCount = 0
+                                playButtonImage.setImageResource(R.drawable.play_icon)
+                            }
+                        } else {
+                            timerState = TimerState.SHORT_BREAK
+                            startTimer(shortBreakTime * 60 * 1000L)
+                        }
+                    }
+                    TimerState.SHORT_BREAK -> {
+                        timerState = TimerState.FOCUS
+                        startTimer(focusTime * 60 * 1000L)
+                    }
+                    TimerState.LONG_BREAK -> {
+                        timerState = TimerState.FOCUS
+                        startTimer(focusTime * 60 * 1000L)
+                    }
                 }
-                startTimer(timeLeftInMillis)
             }
         }.start()
-
         isTimerRunning = true
+    }
+
+    private fun updateCountDownText() {
+        val minutes = (timeLeftInMillis / 1000) / 60
+        val seconds = (timeLeftInMillis / 1000) % 60
+        val timeLeftFormatted = String.format("%02d\n%02d", minutes, seconds)
+        timerDisplay.text = timeLeftFormatted
+
+        // Change text color and background color based on timer state
+        val textColor: Int
+        when (timerState) {
+            TimerState.FOCUS -> {
+                textColor = R.color.focusColor
+            }
+            TimerState.SHORT_BREAK -> {
+                textColor = R.color.shortBreakColor
+            }
+            TimerState.LONG_BREAK -> {
+                textColor = R.color.longBreakColor
+            }
+        }
+        timerDisplay.setTextColor(ContextCompat.getColor(requireContext(), textColor))
     }
 
     private fun pauseTimer() {
@@ -142,16 +179,40 @@ class FragmentTimer : Fragment() {
         startTimer(timeLeftInMillis)
     }
 
+    private fun skipTimer() {
+        timer?.cancel()
+        timer = null
+        isTimerRunning = false
+        when (timerState) {
+            TimerState.FOCUS -> {
+                completedFocusSessions++
+                if (completedFocusSessions % pomoCount == 0) {
+                    if (cycleCount < pomoCycleCount - 1) {
+                        timerState = TimerState.LONG_BREAK
+                        startTimer(longBreakTime * 60 * 1000L)
+                        cycleCount++
+                    } else {
+                        Toast.makeText(context, "All cycles completed!", Toast.LENGTH_SHORT).show()
+                        timerState = TimerState.FOCUS
+                        cycleCount = 0
+                    }
+                } else {
+                    timerState = TimerState.SHORT_BREAK
+                    startTimer(shortBreakTime * 60 * 1000L)
+                }
+            }
+            TimerState.SHORT_BREAK -> {
+                timerState = TimerState.FOCUS
+                startTimer(focusTime * 60 * 1000L)
+            }
+            TimerState.LONG_BREAK -> {
+                timerState = TimerState.FOCUS
+                startTimer(focusTime * 60 * 1000L)
+            }
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentTimer.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             FragmentTimer().apply {
